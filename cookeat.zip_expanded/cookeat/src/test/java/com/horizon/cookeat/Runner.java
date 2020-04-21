402dc8b5da16468687aad0d2e52cacfe781a4592
unknown <tchong.kite.h@gmail.com>
2020-04-21 18:32:37 +0200

    using hibernate

---
+++ cookeat.zip_expanded/cookeat/src/test/java/com/horizon/cookeat/Runner.java
@@ -0,0 +1,108 @@
+package com.horizon.cookeat;
+
+import java.util.List;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.junit.jupiter.api.Test;
+
+import com.horizon.cookeat.model.Recipe;
+
+@SuppressWarnings("deprecation")
+public class Runner {
+    
+    @Test
+    public void crud() {
+    	try
+    	{		
+//    		SessionFactory sessionFactory = configuration.buildSessionFactory();
+	    	SessionFactory sessionFactory = new AnnotationConfiguration().configure().buildSessionFactory();
+	        Session session = sessionFactory.openSession();
+	        
+	        create(session);
+	        read(session);
+	        
+	        update(session);
+	        read(session);
+	        
+	        delete(session);
+	        read(session);
+	         
+	        session.close();
+    	}
+    	catch(Exception e) { Utils.log.debug("Crud: " + e.getMessage()); }
+    }
+     
+    private void delete(Session session) {
+    	try
+    	{    		
+    		Utils.log.debug("Deleting mondeo record...");
+	        Recipe trucDegueu = (Recipe) session.get(Recipe.class, "3");
+	         
+	        session.beginTransaction();
+	        session.delete(trucDegueu);
+	        session.getTransaction().commit();
+    	}
+    	catch(Exception ex) { Utils.log.debug("Delete: " + ex.getMessage()); }
+    }
+     
+    private void update(Session session) {
+        try {	        	
+        	Utils.log.debug("Updating tarte aux pommes price...");
+	        Recipe tartopom = (Recipe) session.get(Recipe.class, "2");
+	        tartopom.setTotal_price(2000);
+	         
+	        session.beginTransaction();
+	        session.saveOrUpdate(tartopom);
+	        session.getTransaction().commit();
+        }
+        catch (Exception e) { Utils.log.debug("Update: " +e.getMessage()); }
+    }
+ 
+    private void create(Session session) {
+    	try
+    	{
+	    	Utils.log.debug("Creating car recipes...");
+	        
+	        Recipe patesCarbo = new Recipe();
+	        patesCarbo.setId(1);
+	        patesCarbo.setDesignation("Pates a la carbonara");
+	        patesCarbo.setPrep_time(15);
+	        patesCarbo.setTotal_price(500);
+	        
+	        Utils.log.debug("Created Pates a la carbonara");
+	        
+	        Recipe tarteAuxPomxml = new Recipe();
+	        tarteAuxPomxml.setId(2);
+	        tarteAuxPomxml.setDesignation("Tarte aux pommes");
+	        tarteAuxPomxml.setPrep_time(90);
+	        tarteAuxPomxml.setTotal_price(1500);
+	        
+	        Utils.log.debug("Created Tarte aux pommes");
+	        
+	         
+	        session.beginTransaction();
+	        session.save(patesCarbo);
+	        session.save(tarteAuxPomxml);
+	        session.getTransaction().commit();
+    	}
+    	catch(Exception e) { Utils.log.debug("Create: " + e.getMessage()); }
+    }
+     
+    private void read(Session session) {
+    	try
+    	{
+	        Query q = session.createQuery("select recipe from Car _car");
+	         
+	        List<Recipe> recipes = q.list();
+	        Utils.log.debug("Reading recipes in database...");
+	        for (Recipe r : recipes) {
+	        	Utils.log.debug("#%s - %s", r.getId(), r.getDesignation());
+	        }
+    	}
+    	catch(Exception e) {Utils.log.debug("Read: " + e.getMessage()); }
+    }
+}
