5d64e16a060bf90510345a50e8d9bf7124a8edd4
Jules Ponsaillé <jules.ponsaille@isep.fr>
2020-05-08 18:45:38 +0200

    adding basic infos to the basket

--- front/src/components/basket/BasketItem.js
+++ front/src/components/basket/BasketItem.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ListItem, ListItemText, ListItemAvatar, Avatar } from '@material-ui/core';
 
-function BasketItem({ designation, list_gallery }) {
+function BasketItem({ designation, list_gallery, total_price, prep_time }) {
     return (
         <ListItem>
             <ListItemAvatar>
@@ -9,6 +9,7 @@ function BasketItem({ designation, list_gallery }) {
             </ListItemAvatar>
             <ListItemText
                 primary={designation}
+                secondary={`${total_price}€ - ${prep_time}min `}
             />
         </ListItem>
     )
