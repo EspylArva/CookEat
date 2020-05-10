8c228210c6a496cfedb53dbbe26a0ef6da263bd4
Batelier <baptiste.roberjot@gmail.com>
2020-05-10 15:39:04 +0200

    working carousel

--- front/src/components/recipe/recipe_informationScreen.js
+++ front/src/components/recipe/recipe_informationScreen.js
@@ -4,32 +4,33 @@ import HourglassEmptyIcon from '@material-ui/icons/HourglassEmpty';
 import { Chip, CardMedia } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core';
 import AwesomeSlider from 'react-awesome-slider';
-import img1 from 'coquillettes_1.jpg'
+import 'react-awesome-slider/dist/styles.css';
+
 
 const total_price = 2;
 const prep_time = 15;
-/*
+
 const useStyles = makeStyles({
     chip:{
         backgroundColor: "white",
         padding: "5px",
     }
 });
-*/
+
 
 function getInformationScreen(){
     //const classes = useStyles();
     return(
         <div>
             <CardMedia
-            image={img1}>
+            >
 
             </CardMedia>
             <AwesomeSlider>
-                <div data-src="./components/local_test_res/coquillettes_2.jpg" />
-            
+                <div data-src="/local_test_res/coquillettes_1.jpg" />
+                <div data-src="/local_test_res/coquillettes_2.jpg" />
+                <div data-src="/local_test_res/coquillettes_3.jpg" />
             </AwesomeSlider>
-            <p icon={<EuroIcon/>}> 2</p>
             <Chip label={prep_time + "min"} icon={<HourglassEmptyIcon />} />
             <Chip label={total_price} icon={<EuroIcon />} />
         </div>
