ee73310b264117a6acbbc0a4fd52c35e595e635d
Jules Ponsaillé <jules.ponsaille@isep.fr>
2020-05-23 12:08:08 +0200

    state messages styles for basket

--- front/src/components/basket/BasketList.js
+++ front/src/components/basket/BasketList.js
@@ -1,27 +1,58 @@
 import React from 'react';
-import { List } from '@material-ui/core';
+import { makeStyles,List } from '@material-ui/core';
+import { CircularProgress } from '@material-ui/core';
+import CloudOffIcon from '@material-ui/icons/CloudOff';
 import { Link } from 'react-router-dom';
 import BasketItem from './BasketItem';
 import useCookeatDB from '../../hooks/useCookEatDB';
 
+const useStyles = makeStyles({
+    stateIndicator: {
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100%"
+    },
+    stateIndicatorMessage: {
+        margin: "20px",
+        fontSize: "1.2em",
+        fontWeight: "bold",
+        color: "gray",
+        textAlign: "center"
+    }
+});
+
 function BasketList() {
+    const classes  = useStyles();
+
     const [basket, loading, error, actions] = useCookeatDB('basketRecipes');
 
     if(error) {
         return (
-            <em>Il y a eu une erreur lors du chargement du panier. En attendant <Link to="/search">vous pouvez aller matcher ici</Link></em>
+            <div className={classes.stateIndicator}>
+                <CloudOffIcon fontSize="large" />
+                <em className={classes.stateIndicatorMessage}>Oups ! Il semblerait que nous n'avons pas réussi à récupérer le panier. Essayez de recharger la page.</em>
+            </div>
         )
     }
 
     if(!basket || !basket.length) {
         if(loading) {
             return (
-                <em>Nous chargeons vos recettes sauvagés depuis votre téléphone</em>
+                <div className={classes.stateIndicator}>
+                    <CircularProgress size={100} />
+                    <em className={classes.stateIndicatorMessage}>Nous chargeons vos recettes sauvegardés sur votre système.</em>
+                </div>
             )
         }
 
+        
+
         return (
-            <em>Vous n'avez pas de recette dans votre panier. <Link to="/search">Vous pouvez aller matcher ici</Link></em>
+            <div className={classes.stateIndicator}>
+                <em className={classes.stateIndicatorMessage}>Vous n'avez pas de recette dans votre panier. <Link to="/search">Vous pouvez aller matcher ici</Link></em>
+            </div>
         )
     }
 
