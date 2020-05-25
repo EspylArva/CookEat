cb9a90a8069fca0dd5889b817a832cf3545800f7
Batelier <baptiste.roberjot@isep.fr>
2020-05-25 10:30:28 +0200

    added season information screen

--- front/src/components/recipe/InformationScreen.js
+++ front/src/components/recipe/InformationScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import EuroIcon from '@material-ui/icons/Euro';
 import HourglassEmptyIcon from '@material-ui/icons/HourglassEmpty';
+import CalendarTodayIcon from '@material-ui/icons/CalendarToday';
 import { Chip } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core';
 import AwesomeSlider from 'react-awesome-slider';
@@ -20,29 +21,35 @@ const useStyles = makeStyles({
         maxWidth:"100px"
     }
 });
+var months = [ "Janvier", "Février", "Mars", "Avril", "Mai", "Juin", 
+           "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre" ];
 
 
 function InformationScreen(
     {designation,
     total_price,
     prep_time,
-    list_gallery}
+    list_gallery,
+    start_season,
+    end_season}
 ){
     let { recipeID } = useParams();
     const classes = useStyles();
     //make a max width 
     //<div data-src={list_gallery[0]}/>
     //{list_gallery.map((item) =>(<div>{item}</div>))}
+    const period = "De " + months[start_season] + " à " + months[end_season]
     console.log(list_gallery)
     return(
         <div>
             
             <AwesomeSlider className={classes.img}> 
-                
+                {list_gallery.map((item) =>(<div>{item.path}</div>))}
             </AwesomeSlider>
             
             <Chip  className={classes.chip} label={prep_time + "min"} icon={<HourglassEmptyIcon />} variant="outlined"/><br/>
-            <Chip  className={classes.chip} label={total_price} icon={<EuroIcon />} variant="outlined" />
+            <Chip  className={classes.chip} label={total_price} icon={<EuroIcon />} variant="outlined" /><br/>
+            <Chip  className={classes.chip} label={period} icon={<CalendarTodayIcon />} variant="outlined" />
         </div>
     )
 }
