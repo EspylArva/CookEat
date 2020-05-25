32d1496b08480075f178a9a0f74037e70fa2f00f
Batelier <baptiste.roberjot@isep.fr>
2020-05-25 13:23:59 +0200

    added season alert

--- front/src/components/recipe/InformationScreen.js
+++ front/src/components/recipe/InformationScreen.js
@@ -8,6 +8,8 @@ import AliceCarousel from 'react-alice-carousel';
 import "react-alice-carousel/lib/alice-carousel.css";
 import CloseIcon from '@material-ui/icons/Close';
 import DoneIcon from '@material-ui/icons/Done';
+import Alert from 'react-bootstrap/Alert'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const useStyles = makeStyles({
     chip:{
@@ -31,7 +33,7 @@ const useStyles = makeStyles({
     },
     doneIconCSS:{
         color: 'green'
-    }
+    },
 });
 var months = [ "January", "February", "March", "April", "May", "June", 
            "July", "August", "September", "October", "November", "December" ];
@@ -73,6 +75,11 @@ function InformationScreen(
     const period = "From " + months[start_season] + " to " + months[end_season]
     return(
         <div className={classes.imgContainerDiv}>
+            {(goodMonth)
+                ? (<Alert variant='success'>It's the right season to make this recipe.</Alert>)
+                : (<Alert variant='danger'>This is not the right season to make this recipe!</Alert>)
+
+            }
             <hr/>
             <Chip  className={classes.chip} label={prep_time + "min"} icon={<HourglassEmptyIcon />} variant="outlined"/><span>&emsp;</span>
             <Chip  className={classes.chip} label={total_price} icon={<EuroIcon />} variant="outlined" /><span>&emsp;</span>
@@ -80,12 +87,8 @@ function InformationScreen(
                 ? (<Chip  className={classes.chip} label={period} icon={<DoneIcon className={classes.doneIconCSS}/>} variant="outlined" />)
                 : ( <Chip  className={classes.chip} label={period} icon={<CloseIcon className={classes.closeIconCSS}/>} variant="outlined" /> )
             }
-            
-               
-              
             <hr/>
- 
-            
+  
             <AliceCarousel  autoPlay autoPlayInterval="5000" className={classes.img}>
                 {list_gallery.map((item) =>(
                     <img src={item.path} className={classes.sliderimg} />
