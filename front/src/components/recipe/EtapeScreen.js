d85d43a232f525fd40edf1634005d497952ae6b2
Batelier <baptiste.roberjot@isep.fr>
2020-05-25 11:15:28 +0200

    fixing stepscreen issue

--- front/src/components/recipe/EtapeScreen.js
+++ front/src/components/recipe/EtapeScreen.js
@@ -1,15 +1,23 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core';
 
+
+const useStyles = makeStyles({
+    mainDiv:{
+        margin: '0 0 10% 0'
+    }
+})
 
 function EtapeScreen(
     {list_steps}
 ){
+    const classes = useStyles();
     {list_steps.map((item, index) =>(
         console.log(item)
     ))
     }
     return(
-        <div>
+        <div className={classes.mainDiv}>
             {list_steps.map((item, index) =>(
                <div>
                    <h4>Etape {item.step_order + 1}</h4>
